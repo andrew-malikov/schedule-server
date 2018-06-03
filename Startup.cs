cec9379235113a52a4a891a528668ecbddf13c36
Andrew <andrew.devels@gmail.com>
2018-06-03 15:46:14 +0500

    Enable global CORS

--- Startup.cs
+++ Startup.cs
@@ -102,6 +102,15 @@ namespace ScheduleServer {
             services.AddSingleton<IHostedService, UpdateUniversityDbService>();
 
             services.AddMvc();
+            
+            services.AddCors(options => {
+                options.AddPolicy("AllowAll", builder => {
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
